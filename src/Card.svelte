e1e9f19be5192548c07e51e16afef26c89462cfc
Dennis Melzer <melzer.dennis@gmail.com>
2025-07-16 08:46:20 +0200

    Update Codestyle Card.svelte
    Update Codestyle for Card.svelte to remove warning from linter

--- src/Card.svelte
+++ src/Card.svelte
@@ -1,12 +1,12 @@
 <!--
 /*
 Copyright 2021-2022 Peter Repukat - FlatspotSoftware
-Licensed under the Apache License, Version 2.0 (the "License");
+Licensed under the Apache License, Version 2.0 (the 'License');
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 http://www.apache.org/licenses/LICENSE-2.0
 Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
+distributed under the License is distributed on an 'AS IS' BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
@@ -15,7 +15,7 @@ limitations under the License.
 <!-- eslint-disable-next-line svelte/valid-compile -->
 <svelte:options customElement='expander-sub-card' />
 
-<script lang="ts">
+<script lang='ts'>
     import type { LovelaceCard, HomeAssistant, LovelaceCardConfig } from 'custom-card-helpers';
     import { getCardUtil } from './cardUtil.svelte';
     import { onMount } from 'svelte';
@@ -39,10 +39,10 @@ limitations under the License.
     });
     $effect(() => {
         const conditions = [{
-            condition: "screen",
-            media_query: open ? "(max-width: 99999px)" : "(max-width: 0px)"
+            condition: 'screen',
+            media_query: open ? '(max-width: 99999px)' : '(max-width: 0px)'
         }];
-        const card = { type: "conditional", conditions: conditions, card: config } as LovelaceCardConfig;
+        const card = { type: 'conditional', conditions: conditions, card: config } as LovelaceCardConfig;
         // setConfig exists on condition-card but without ?. svelte will not find it.
         container?.setConfig?.(card);
     });
@@ -50,10 +50,10 @@ limitations under the License.
     onMount(async () => {
         const util = await getCardUtil();
         const conditions = [{
-            condition: "screen",
-            media_query: open ? "(max-width: 99999px)" : "(max-width: 0px)"
+            condition: 'screen',
+            media_query: open ? '(max-width: 99999px)' : '(max-width: 0px)'
         }];
-        const card = { type: "conditional", conditions: conditions, card: config };
+        const card = { type: 'conditional', conditions: conditions, card: config };
         const el = util.createCardElement(card);
         el.hass = hass;
 
@@ -67,10 +67,10 @@ limitations under the License.
     });
 </script>
 
-<div class="outer-container" style="margin-top: {marginTop};">
+<div class='outer-container' style='margin-top: {marginTop};'>
     <svelte:element this={type} bind:this={container} transition:slide|local />
     {#if loading}
-        <span class="loading"> Loading... </span>
+        <span class='loading'> Loading... </span>
     {/if}
 </div>
 
