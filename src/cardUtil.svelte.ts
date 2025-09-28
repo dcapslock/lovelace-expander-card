dc718c9139da47b929953bf2ef208a211f5cb004
dcapslock <dcapes-davis@outlook.com.au>
2025-09-28 12:27:17 +1000

    Use diabled card config available in HA 2025.10

--- src/cardUtil.svelte.ts
+++ src/cardUtil.svelte.ts
@@ -11,7 +11,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import type { LovelaceCard, LovelaceCardConfig } from 'custom-card-helpers';
+import type { LovelaceCard, LovelaceCardConfig } from './types';
 
 export interface CardUtil {
     createCardElement: (config: LovelaceCardConfig) => LovelaceCard;
